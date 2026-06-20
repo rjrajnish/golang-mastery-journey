9728b2a97d01d259c86811d0c4bab9834e91fc42
Rjrajnish99 <149811471+Rjrajnish99@users.noreply.github.com>
2026-06-20 23:28:36 +0530

    add multiple declarations

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -3,22 +3,21 @@ package main
 import "fmt"
 
 func main() {
-
-	// Variable Declaration
-	var name string = "Rajnish"
-	var age int = 25
-	var salary float64 = 50000.50
-	var isDeveloper bool = true
-
-	fmt.Println("Name:", name)
-	fmt.Println("Age:", age)
-	fmt.Println("Salary:", salary)
-	fmt.Println("Developer:", isDeveloper)
-
-	// Short Variable Declaration
-	city := "Ghaziabad"
+// basic variables
+	var name string ="Rajnish"
+	var age int=25
+    var salary float64=9000.56
+	var isDeveloper bool= true
+	fmt.Println("Name : ",name)
+	fmt.Println("Age : ", age)
+	fmt.Println("Salary : ",salary)
+	fmt.Println("Is This Developer :", isDeveloper)
+	// short declarations
+	city := "New Delhi"
 	country := "India"
 
-	fmt.Println("City:", city)
-	fmt.Println("Country:", country)
+	fmt.Print("Location:",city,country)
+	// multiple declarations
+	new_name,new_age,new_salary,new_city,new_country := "Rajnish",25,9000.56,"noida","India"
+	fmt.Print("multiple ",new_name,new_age,new_salary,new_city,new_country)
 }
